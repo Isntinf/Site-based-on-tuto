b04ee7eeceed459d0516920268c923b265f52913
Kacper <kaperq01@gmail.com>
2022-06-25 12:51:10 +0200

    add animations

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/* It's selecting the card element and assigning it to the card variable. */
+const card = document.querySelector(".card")
+
+/* It's a GSAP animation. */
+
+const tl = new TimelineMax({ delay: 0.5 });
+
+tl.from(card, 1, { opacity: 0 })
+    .from(card, 1, { color: '#4d4d4d' })
+
+/* It's a JavaScript object. */
+
 let weather = {
     apiKey: "06c921750b9a82d8f5d1294e1586276f",
     fetchWeather: function(city) {
@@ -9,6 +21,7 @@ let weather = {
             ).then((response) => response.json())
             .then((data) => this.displayWeather(data));
     },
+    /* It's a JavaScript object. */
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
@@ -24,7 +37,7 @@ let weather = {
         document.querySelector(".wind").innerText =
             "Wind speed: " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
-                    document.body.style.backgroundImage =
+        document.body.style.backgroundImage =
             "url('https://source.unsplash.com/1920x1080/?" + "landscape" + "')";
     },
     search: function() {
@@ -34,20 +47,27 @@ let weather = {
     },
 };
 
+/* It's a JavaScript event listener. It's listening for a click event on the search button. When the
+button is clicked, it calls the search function. */
 document.querySelector(".search button").addEventListener("click", function() {
     weather.search();
 });
 
+/* It's listening for a keyup event on the search bar. When the enter key is pressed, it calls the
+search function. */
 document.querySelector(".search-bar").addEventListener("keyup", function(event) {
     if (event.key == "Enter") {
         weather.search();
     }
 })
 
+/* It's listening for a click event on the search button. When the button is clicked, it calls the
+search function. */
 document
     .querySelector(".search buutton")
     .addEventListener("click", function() {
         weather.search();
     })
+    /* It's calling the fetchWeather function and passing in the string "Bogatynia" as the city parameter. */
 
-weather.fetchWeather("Bogatynia")
+weather.fetchWeather('Bogatynia')
